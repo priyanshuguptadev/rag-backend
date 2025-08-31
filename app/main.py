cee9521633ba112ff0b60340635a8525193f0398
Priyanshu Gupta <realpriyanshugupta@gmail.com>
2025-09-01 00:25:23 +0530

    add cors

--- app/main.py
+++ app/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routes import answer
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+FRONTEND_URL = os.getenv("FRONTEND_URL")
 
 app = FastAPI()
+
+origins = [FRONTEND_URL]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["GET", "POST"],
+    allow_headers=["Content-Type"],
+)
 
 app.include_router(answer.router)
 
